eaadcbcb3b0fed5ae29c0d7555a476295adf4eda
Pouria Delfanazari <pdelfan@gmail.com>
2023-07-31 18:47:10 -0700

    Update landing page with a new design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
 import Image from "next/image";
-import LandingBackground from "@/assets/images/landingBackgroundBlurred.png";
+import LandingImage from "@/assets/images/landingImage.png";
 import Logo from "@/assets/logo.svg";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex items-center align-middle justify-center min-h-screen ">
-      <Image
-        className=" object-cover animate-fade animate-duration-200"
-        src={LandingBackground}
-        alt="Wide shot of a mountain covered in snow"
-        fill={true}
-      />
-      <section className="relative flex flex-col place-items-center justify-center text-center p-3 animate-fade animate-duration-200">
+    <main className="flex items-center justify-center">
+      <section className="relative flex flex-col place-items-center justify-center text-center p-3 mt-5 animate-fade animate-duration-200">
         <Image
           src={Logo}
           alt="Holos logo"
@@ -20,10 +14,10 @@ export default function Home() {
           height={60}
           className="animate-fade-down animate-duration-700 animate-delay-100"
         />
-        <h1 className="text-white text-4xl sm:text-5xl font-semibold mt-3 animate-fade-down animate-duration-700 animate-delay-150">
-          Head into the wild, prepared
+        <h1 className="text-header-1 text-4xl sm:text-5xl font-medium mt-3 animate-fade-down animate-duration-700 animate-delay-150">
+          Head into the wild, <em>prepared</em>
         </h1>
-        <h2 className="text-white text-md sm:text-lg font-semibold mt-3 max-w-full sm:max-w-lg animate-fade-down animate-duration-700 animate-delay-150">
+        <h2 className="text-header-2 text-md sm:text-lg font-medium mt-3 max-w-full sm:max-w-lg animate-fade-down animate-duration-700 animate-delay-150">
           Plan your hikes with confidence. Organize your gear essentials,
           prioritize must-haves, and build the perfect pack for every trek.
         </h2>
@@ -44,6 +38,12 @@ export default function Home() {
             Sign In
           </Link>
         </div>
+        <Image
+          className="animate-fade-up mt-12"
+          src={LandingImage}
+          alt="Illustration of hikers on a path that is made up of the letters H-O-L-O-S"
+          width={900}
+        />
       </section>
     </main>
   );
