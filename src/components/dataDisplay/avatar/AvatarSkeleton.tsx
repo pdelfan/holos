ebf7cbc956725a0dc668dad9b289ce24fd7b78d1
Pouria Delfanazari <pdelfan@gmail.com>
2023-09-25 12:14:33 -0700

    Remove hardcoded username

--- src/components/dataDisplay/avatar/AvatarSkeleton.tsx
+++ src/components/dataDisplay/avatar/AvatarSkeleton.tsx
@@ -6,9 +6,9 @@ export default function AvatarSkeleton(props: Props) {
   const { size = "small" } = props;
   return (
     <span
-      className={`flex justify-center items-center ${
-        size === "small" ? "h-8 w-8 rounded-lg" : "h-24 w-24 rounded-3xl"
-      } rounded-lg bg-gray-300 animate-pulse`}
+      className={`flex justify-center items-center rounded-full ${
+        size === "small" ? "h-8 w-8" : "h-24 w-24"
+      } bg-gray-300 animate-pulse`}
     >
       <span className="text-sm text-amber-50 animate-pulse" />
     </span>
--- src/components/navigational/header/Header.tsx
+++ src/components/navigational/header/Header.tsx
@@ -10,10 +10,12 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "@/lib/database.types";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import useGetUser from "@/hooks/useGetUser";
 
 function Header() {
   const supabase = createClientComponentClient<Database>();
   const router = useRouter();
+  const { user } = useGetUser();
 
   const onSignOut = async () => {
     const { error } = await supabase.auth.signOut();
@@ -29,14 +31,10 @@ function Header() {
   return (
     <header className="sticky z-50 top-0 left-0 right-0 flex items-center justify-between bg-white px-3 py-2 border-b">
       <Link href="/dashboard">
-        <Image
-          src={Logo}
-          alt="HOLOS logo"
-          width={40}          
-        />
+        <Image src={Logo} alt="HOLOS logo" width={40} />
       </Link>
       <Navbar />
-      <Dropdown button={<Avatar name="Pouria" />}>
+      <Dropdown button={<Avatar name={user} />}>
         <DropdownItem onClick={navigateToSettings}>Settings</DropdownItem>
         <DropdownItem onClick={onSignOut}>Sign Out</DropdownItem>
       </Dropdown>
