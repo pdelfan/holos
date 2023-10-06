a2f1cfd927f052250dee58e718ba3b9b33f4467e
Pouria Delfanazari <pdelfan@gmail.com>
2023-10-05 20:22:39 -0700

    Add dark mode for card skeletons

--- src/components/dataDisplay/avatar/AvatarSkeleton.tsx
+++ src/components/dataDisplay/avatar/AvatarSkeleton.tsx
@@ -8,7 +8,7 @@ export default function AvatarSkeleton(props: Props) {
     <span
       className={`flex justify-center items-center rounded-full ${
         size === "small" ? "h-8 w-8" : "h-24 w-24"
-      } bg-gray-300 animate-pulse`}
+      } bg-gray-300 animate-pulse dark:bg-gray-600`}
     >
       <span className="text-sm text-amber-50 animate-pulse" />
     </span>
