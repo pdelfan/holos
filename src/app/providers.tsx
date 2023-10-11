24bce1dd54b329f944a23f6559436591d5350087
Pouria Delfanazari <pdelfan@gmail.com>
2023-10-10 17:14:39 -0700

    Add auth provider to listen to sign out event on all tabs

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,18 +2,38 @@
 
 import { Toaster } from "react-hot-toast";
 import { Provider } from "jotai";
+import { useEffect } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Database } from "@/lib/database.types";
+import { useRouter } from "next/navigation";
 
 interface Props {
   children: React.ReactNode;
+}
+
+function AuthProvider(props: Props) {
+  const { children } = props;
+  const supabase = createClientComponentClient<Database>();
+  const router = useRouter();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event) => {
+      if (event === "SIGNED_OUT") {
+        router.push("/");
+      }
+    });
+  }, [router, supabase.auth]);
+
+  return <>{children}</>;
 }
 
 export default function Providers(props: Props) {
   const { children } = props;
 
   return (
-    <>
+    <AuthProvider>
       <Toaster />
       <Provider>{children}</Provider>
-    </>
+    </AuthProvider>
   );
 }
