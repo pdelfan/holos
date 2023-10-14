904f51e89659238abe161e65aef3cbc5770e7093
Pouria Delfanazari <pdelfan@gmail.com>
2023-10-13 21:35:42 -0700

    Add loading skeleton on settings page

--- next.config.js
+++ next.config.js
@@ -6,13 +6,13 @@ const nextConfig = {
         protocol: "https",
         hostname: "**",
         port: "",
-        pathname: "/**",
+        pathname: "**",
       },
       {
         protocol: "http",
         hostname: "**",
         port: "",
-        pathname: "/**",
+        pathname: "**",
       },
       {
         protocol: "http",
