16b5cc43175852ea7a56bd9cbd04b2debebb1797
Pouria Delfanazari <pdelfan@gmail.com>
2023-10-16 12:00:34 -0700

    Replace useeffect with useswr for fetching items in grids

--- src/hooks/useFetchDB.tsx
+++ src/hooks/useFetchDB.tsx
@@ -1,11 +1,12 @@
 import { Database } from "@/lib/database.types";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import useSWR from "swr";
 
 interface Props<T> {
   itemPerPage: number;
-  setData: (value: T) => void;
   table: string;
+  setData: (value: T) => void;
 }
 
 export default function useFetchDB<T extends Record<string, any>>(
@@ -13,54 +14,48 @@ export default function useFetchDB<T extends Record<string, any>>(
 ) {
   const { table, itemPerPage, setData } = props;
   const supabase = createClientComponentClient<Database>();
-  const [error, setError] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
   const [pageIndex, setPageIndex] = useState(0);
-  const [totalItems, setTotalItems] = useState(0);
 
-  useEffect(() => {
-    const getCount = async () => {
-      const { data: user } = await supabase.auth.getSession();
-      if (!user.session) {
-        return;
-      }
-      
-      const { count } = await supabase
-        .from(table)
-        .select("*", { count: "exact", head: true })
-        .eq("user_id", user.session?.user.id);
-      setTotalItems(count ?? 0);
-    };
-    getCount();
-  }, [supabase, table]);
+  const fetcher = async () => {
+    const { data: user } = await supabase.auth.getSession();
+    const from = pageIndex * itemPerPage;
+    const to = from + itemPerPage - 1;
 
-  useEffect(() => {
-    const getData = async () => {
-      setError(false);
-      setIsLoading(true);
+    const { data: fetchedData, error } = await supabase
+      .from(table)
+      .select("*")
+      .range(from, to)
+      .eq("user_id", user.session?.user.id);
 
-      const from = pageIndex * itemPerPage;
-      const to = from + itemPerPage - 1; // adjust the range to exclude the extra item
+    if (error) {
+      throw error;
+    }
+    setData(fetchedData as unknown as T);
+    return fetchedData as unknown as T;
+  };
 
-      const { data: user } = await supabase.auth.getSession();
-      const { data: fetchedData, error } = await supabase
-        .from(table)
-        .select("*")
-        .range(from, to)
-        .eq("user_id", user.session?.user.id);
+  const { data, error: fetchError } = useSWR(
+    [table, pageIndex, itemPerPage],
+    fetcher
+  );
 
-      if (error) {
-        setError(true);
-        setIsLoading(false);
-        return;
-      }
-      setData(fetchedData as unknown as T);
-      setIsLoading(false);
-    };
-    getData();
-  }, [itemPerPage, pageIndex, setData, supabase, table]);
+  const countFetcher = async () => {
+    const { data: user } = await supabase.auth.getSession();
+    const { count } = await supabase
+      .from(table)
+      .select("*", { count: "exact", head: true })
+      .eq("user_id", user.session?.user.id);
+    return count;
+  };
 
+  const { data: count, error: countError } = useSWR(
+    [table, "pagniation"],
+    countFetcher
+  );
+
+  const totalItems = count ?? 0;
   const totalPages = Math.ceil(totalItems / itemPerPage);
+  const isLoading = !data && !fetchError;
 
   return {
     totalItems,
@@ -68,7 +63,7 @@ export default function useFetchDB<T extends Record<string, any>>(
     totalPages,
     pageIndex,
     setPageIndex,
-    error,
+    error: fetchError,
     isLoading,
   };
 }
