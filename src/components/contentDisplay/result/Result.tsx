d21465f005dbfbcd2fde206605661d04cab28405
Pouria Delfanazari <pdelfan@gmail.com>
2023-10-16 14:45:33 -0700

    Refactor result messages into a component

---
+++ src/components/contentDisplay/result/Result.tsx
@@ -0,0 +1,19 @@
+interface Prop {
+  status: "error" | "info";
+  children: React.ReactNode;
+}
+
+export default function Result(props: Prop) {
+  const { children } = props;
+  const selectedColour =
+    props.status === "error"
+      ? "text-red-500 dark:text-red-400"
+      : "text-gray dark:text-neutral-400";
+  return (
+    <div className="flex h-full items-center">
+      <h3 className={`text-lg text-center basis-full ${selectedColour}`}>
+        {children}
+      </h3>
+    </div>
+  );
+}
