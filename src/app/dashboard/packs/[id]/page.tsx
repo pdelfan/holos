f0b72d47626323bbae82af0491df9c1a8ff53b8a
Pouria Delfanazari <pdelfan@gmail.com>
2023-10-17 16:30:20 -0700

    Add a not found status when a pack isn't found

--- src/app/dashboard/packs/[id]/page.tsx
+++ src/app/dashboard/packs/[id]/page.tsx
@@ -6,6 +6,9 @@ import ChartSummary from "@/components/dataDisplay/chartSummary/ChartSummary";
 import Table from "@/components/dataDisplay/table/Table";
 import Modal from "@/components/feedback/modal/Modal";
 import GroupForm from "@/components/forms/groupForm/GroupForm";
+import Image from "next/image";
+import FallingIcon from "@/assets/icons/fallingIcon.svg";
+import Link from "next/link";
 import useGetPack from "@/hooks/useGetPack";
 import useGetPreferredCurrency from "@/hooks/useGetPreferredCurrency";
 import { useEffect, useState } from "react";
@@ -47,7 +50,9 @@ interface Props {
 export default function Pack(props: Props) {
   const { params } = props;
   const { pack } = useGetPack(params.id);
-  const { packData, setPackData } = useGetPackData(params.id);
+  const { packData, setPackData, isLoadingPackData } = useGetPackData(
+    params.id
+  );
   const { currency } = useGetPreferredCurrency({});
   const [viewMode, setViewMode] = useState(false);
   const [showAddGroupModal, setShowAddGroupModal] = useState(false);
@@ -149,8 +154,27 @@ export default function Pack(props: Props) {
 
   return (
     <>
-      {!pack && <PackSkeleton />}
-      {pack && currency && (
+      {isLoadingPackData && <PackSkeleton />}
+      {!isLoadingPackData && !pack && (
+        <section className="flex flex-col items-center justify-center p-3 h-[85svh]">
+          <Image
+            src={FallingIcon}
+            alt="A person falling"
+            width={80}
+            height={80}
+          />
+          <h1 className="text-header-2 text-center text-lg sm:text-xl font-medium mt-1 dark:text-neutral-100">
+            The pack you are looking for could not be found.
+          </h1>
+          <Link
+            className="mt-6 px-4 py-2 rounded-full font-medium text-sm bg-button text-button-text dark:bg-neutral-700 dark:text-neutral-200 hover:brightness-95"
+            href={`${process.env.NEXT_PUBLIC_SITE_URL}/dashboard/packs`}
+          >
+            Return Home
+          </Link>
+        </section>
+      )}
+      {!isLoadingPackData && pack && currency && (
         <>
           <section className="flex flex-wrap justify-between items-center gap-3">
             <div>
@@ -228,7 +252,7 @@ export default function Pack(props: Props) {
         </>
       )}
       <section className="mt-5">
-        {!viewMode && (
+        {!viewMode && pack && (
           <Button
             bgColor="bg-button dark:bg-neutral-700"
             textColor="text-button-text dark:text-neutral-200"
