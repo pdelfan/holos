ecde7ec21449cf753a7fddd3a1498596cd896a19
Pouria Delfanazari <pdelfan@gmail.com>
2023-10-21 22:01:51 -0700

    Remove unused hooks, update utils

--- src/utils/packUtils.ts
+++ src/utils/packUtils.ts
@@ -1,4 +1,29 @@
 import { convertWeight } from "./numberUtils";
+import WearableIcon from "@/assets/icons/clothingIcon.svg";
+import ConsumableIcon from "@/assets/icons/consumableIcon.svg";
+import GeneralIcon from "@/assets/icons/itemsIcon.svg";
+
+export const getTagColour = (title: string) => {
+  switch (title) {
+    case "Wearable":
+      return "bg-lime-300";
+    case "Consumable":
+      return "bg-pink-300";
+    default:
+      return "bg-stone-200";
+  }
+};
+
+export const getTagIcon = (title: string) => {
+  switch (title) {
+    case "Wearable":
+      return WearableIcon;
+    case "Consumable":
+      return ConsumableIcon;
+    default:
+      return GeneralIcon;
+  }
+};
 
 export const calculateGroupTotals = (
   packData: GroupData[],
